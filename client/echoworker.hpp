b8467fe04dc41c91023372ce7b51149a6ec91e23
binfess <binfess@yandex.ru>
2019-08-27 19:00:04 +0300

    add echo worker class

---
+++ client/echoworker.hpp
@@ -0,0 +1,18 @@
+#ifndef ECHO_TASK_ECHOWORKER_HPP
+#define ECHO_TASK_ECHOWORKER_HPP
+
+
+class EchoWorker
+{
+
+public:
+
+	void doEchoRequest();
+
+private:
+
+
+};
+
+
+#endif //ECHO_TASK_ECHOWORKER_HPP
